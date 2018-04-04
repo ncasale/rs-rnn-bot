6088235dfa83bd325e61d09e114aadd2e9083b85
JStavy <jonathanstav@gmail.com>
2018-04-04 16:00:23 -0400

    Finished Ep2. Mouse tracking added and edge detection refined

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+import pyautogui
 
 def process_img(printscreen):
      processed_img = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
-     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+     processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=300)
      return processed_img
 
-def screen_record(): 
+def main(): 
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+    
     last_time = time.time()
     while(True):
         # 800x600 windowed mode for GTA 5, at the top left position of your main screen.
         # 40 px accounts for title bar. 
         printscreen =  np.array(ImageGrab.grab(bbox=(0,40,765,534)))
         new_screen = process_img(printscreen)
+
+        m_x, m_y = pyautogui.position()
+        print(m_x,m_y)
 
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
@@ -24,4 +32,4 @@ def screen_record():
             cv2.destroyAllWindows()
             break
 
-screen_record()
+main()
