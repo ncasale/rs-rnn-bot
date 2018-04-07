f981ffdc5fb3c574719b8ee03fcc52e4b21638c7
Nate Casale <casalenm924@gmail.com>
2018-04-06 20:39:57 -0400

    Added functionality to save images to file at fixed frame rate

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from PIL import ImageGrab # pip3 install Pillow
 import cv2 #pip install opencv-python
 import time
 import pyautogui # pip install pyautogui
-import win32api #pip install pywin32
 from matplotlib import pyplot as plt #pip install matplotlib
 from math import cos, sin
+import os
+import sys
+import datetime
 
 def detect(feed,image):
     #Scale the image to be 700x700
@@ -32,9 +34,24 @@ def main():
         time.sleep(1)
     
     last_time = time.time()
+
+    #Enter the frame rate you get when you run this
+    frame_rate = 5
+
+    #Current frame 
+    frame_count = 0
+
+    #Create data directory for this run
+    training_data_path = createTrainingDataDir()
+
     while(True):
+        #Increment frame count
+        frame_count += 1
+
         #Set up array to collect screen data with the dimensions listed below. 40px offset to account for navigation bar
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,765,534)))
+        image_x = 960
+        image_y = 660
+        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,image_x, image_y)))
         new_screen = process_img(printscreen)
 
         #collect current mouse location and print it to the console (there are better ways of doing this with pywin32 that we should look into to allow for recording of mouse clicks)
@@ -46,9 +63,31 @@ def main():
 
         #Top line shows the edge detection window. Line below shows color image
         # cv2.imshow('window', new_screen)
-        feed = cv2.imshow('window2',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        image = cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB)
+
+        #Save image to file
+        writeImageToFile(image, frame_count, training_data_path, frame_rate)
+
+        #Show the second window
+        feed = cv2.imshow('window2',image)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
 
+def writeImageToFile(image, frame_count, path, frame_rate):
+    #Create a new directory for this run of images
+    if frame_count % frame_rate == 0:
+        file_name = path + '/image_detection_training_' + str(frame_count) + '.png'
+        cv2.imwrite(file_name, image)
+
+def createTrainingDataDir():
+    #Create a new directory for this run of images
+    now = datetime.datetime.now()
+    #TODO: datetime formatter instead of concat
+    date_string = str(now.year) + str(now.month) + str(now.day) + str(now.hour) + str(now.minute) + str(now.second)
+    path = './training_images/' + date_string
+    os.mkdir(path)
+    return path
+
 main()
+
