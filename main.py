ed8be26f279035635a90f94755d324b2e0a2c043
Nate Casale <casalenm924@gmail.com>
2018-04-10 21:40:03 -0400

    Cleaned up documentation

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ import os
 import sys
 import datetime
 
+"""
 def detect(feed,image):
+
     #Scale the image to be 700x700
     max_dimension = max(image.shape)
     scale = 700/max_dimension
@@ -27,66 +29,42 @@ def detect(feed,image):
 
     #Define filters to filter by color
     min_color = np.array([])
+"""
 
-def process_img(printscreen): #take the BGR image and convert it to greyscale to use for edge detection processing
+def process_img(printscreen):
+    """Take BGR formatted image and convert to greyscale. Used for edge detection processing.
+    
+    Returns:
+        numpy array -- A greyscale image run through a Canny function
+    """
      processed_img = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
      processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=300)
      return processed_img
 
-def main(): 
-    for i in list(range(4))[::-1]:    #Provides a 4s countdown before the recording starts to allow time to click into the window.
-        print(i+1)
-        time.sleep(1)
+
+def writeImageToFile(image, current_frame, path, frame_rate):
+    """Write the passed image to a file
     
-    last_time = time.time()
+    Arguments:
+        image {numpy array} -- the image to write to file
+        current_frame {int} -- current frame
+        path {string} -- path of write file
+        frame_rate {int} -- frame rate of application
+    """
 
-    #Enter the frame rate you get when you run this
-    frame_rate = 5
-
-    #Current frame 
-    frame_count = 0
-
-    #Create data directory for this run
-    training_data_path = createTrainingDataDir()
-
-    while(True):
-        #Increment frame count
-        frame_count += 1
-
-        #Set up array to collect screen data with the dimensions listed below. 40px offset to account for navigation bar
-        image_x = 960
-        image_y = 660
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,image_x, image_y)))
-        new_screen = process_img(printscreen)
-
-        #collect current mouse location and print it to the console (there are better ways of doing this with pywin32 that we should look into to allow for recording of mouse clicks)
-        m_x, m_y = pyautogui.position()
-        print(m_x,m_y)
-
-        print('loop took {} seconds'.format(time.time()-last_time))
-        last_time = time.time()
-
-        #Top line shows the edge detection window. Line below shows color image
-        # cv2.imshow('window', new_screen)
-        image = cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB)
-
-        #Save image to file
-        writeImageToFile(image, frame_count, training_data_path, frame_rate)
-
-        #Show the second window
-        feed = cv2.imshow('window2',image)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-def writeImageToFile(image, frame_count, path, frame_rate):
-    #Create a new directory for this run of images
-    if frame_count % frame_rate == 0:
-        file_name = path + '/image_detection_training_' + str(int(frame_count/frame_rate)) + '.png'
+    #Only write to file once per second
+    if current_frame % frame_rate == 0:
+        file_name = path + '/image_detection_training_' + str(int(current_frame/frame_rate)) + '.png'
         cv2.imwrite(file_name, image)
 
 def createTrainingDataDir():
-    #Create a new directory for this run of images
+    """Creates the directory housing training images if it doesn't exist. The directories are 
+       created in the format: YYYYMMDDHH.   
+    
+    Returns:
+        string -- the path to the training directory
+    """
+    #Get current datetime
     now = datetime.datetime.now()
 
     #Format day/month/hour correctly
@@ -115,7 +93,52 @@ def labelimg():
     6: python3 labelImg.py
     """
     
+def main():
+    #Provide 4 second countdown before recording to navigate to window 
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+    
+    #Time used for loop timing
+    last_time = time.time()
 
+    #Enter the frame rate you get when you run this
+    frame_rate = 5
+
+    #Current frame 
+    current_frame = 0
+
+    #Create data directory for this run
+    training_data_path = createTrainingDataDir()
+
+    while(True):
+        #Increment frame count
+        current_frame += 1
+
+        #Set up array to collect screen data with the dimensions listed below. 40px offset to account for navigation bar
+        image_x = 960
+        image_y = 660
+        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,image_x, image_y)))
+        new_screen = process_img(printscreen)
+
+        #collect current mouse location and print it to the console (there are better ways of doing this with pywin32 that we should look into to allow for recording of mouse clicks)
+        m_x, m_y = pyautogui.position()
+        print(m_x,m_y)
+
+        print('loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+
+        #Top line shows the edge detection window. Line below shows color image
+        # cv2.imshow('window', new_screen)
+        image = cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB)
+
+        #Save image to file
+        writeImageToFile(image, current_frame, training_data_path, frame_rate)
+
+        #Show the second window
+        feed = cv2.imshow('window2',image)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
 main()
-
