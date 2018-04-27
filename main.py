6abd02f38af0a629807f180550908f6d0b47427b
JStavy <jonathanstav@gmail.com>
2018-04-27 15:24:24 -0400

    added basic input tracking. Cleaned up image collection code

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ from math import cos, sin
 import os
 import sys
 import datetime
+import pyHook #pip install pyHook-1.5.1-cp36-cp36m-win_amd64.whl
+import pythoncom # pip install pywin32 -- WINDOWS ONLY
+
 
 """
 def detect(feed,image):
@@ -37,9 +40,9 @@ def process_img(printscreen):
     Returns:
         numpy array -- A greyscale image run through a Canny function
     """
-     processed_img = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
-     processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=300)
-     return processed_img
+    processed_img = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=300)
+    return processed_img
 
 
 def writeImageToFile(image, current_frame, path, frame_rate):
@@ -92,6 +95,20 @@ def labelimg():
     5: make qt5py3 
     6: python3 labelImg.py
     """
+def left_down(event):
+    print ("left click")
+    print ('Position:',event.Position)
+    return True  
+                
+
+def right_down(event):
+    print ("right click")
+    print ('Position:',event.Position)
+    return True    
+    
+def OnKeyboardEvent(event):
+    print (event.Key)
+    return True    
     
 def main():
     #Provide 4 second countdown before recording to navigate to window 
@@ -109,7 +126,10 @@ def main():
     current_frame = 0
 
     #Create data directory for this run
-    training_data_path = createTrainingDataDir()
+    image_count, training_data_path = createTrainingDataDir()
+    current_image = int(image_count)
+    current_frame = int(current_image*frame_rate)
+
 
     while(True):
         #Increment frame count
@@ -122,16 +142,22 @@ def main():
         new_screen = process_img(printscreen)
 
         #collect current mouse location and print it to the console (there are better ways of doing this with pywin32 that we should look into to allow for recording of mouse clicks)
-        m_x, m_y = pyautogui.position()
-        print(m_x,m_y)
+        # m_x, m_y = pyautogui.position()
+        hm.HookMouse()
+        hm.HookKeyboard()
 
+        while(left_down == True):  ## Todo: Right now, this works for registering when a mouse button is clicked but cant register when a key/mouse button is lifted
+            pythoncom.PumpMessages()
+
+
+        #check how long the loop took to run
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
 
         #Top line shows the edge detection window. Line below shows color image
         # cv2.imshow('window', new_screen)
         image = cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB)
-
+        
         #Save image to file
         writeImageToFile(image, current_frame, training_data_path, frame_rate)
 
@@ -139,6 +165,69 @@ def main():
         feed = cv2.imshow('window2',image)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
+            hm.UnhookMouse()
+            hm.UnhookKeyboard()
             break
 
+def writeImageToFile(image, frame_count, path, frame_rate):
+    #Create a new directory for this run of images
+    if frame_count % frame_rate == 0:
+        file_name = path + '/image_detection_training_' + str(int(frame_count/frame_rate)) + '.png'
+        cv2.imwrite(file_name, image)
+
+def createTrainingDataDir():
+    #Create a new directory for this run of images
+    now = datetime.datetime.now()
+    #TODO: datetime formatter instead of concat
+    date_string = str(now.year) + str(now.month) + str(now.day) + str(now.hour)
+    path = './training_images/' + date_string
+    frame_count = 0 
+
+    #Check if a directory exists for this hour. If there is, open the directory and save images to that. If not, create a new directory.
+    if not os.path.exists(path):
+        print('No directory found. Making new directory at ' + date_string)
+        os.makedirs(path)
+        file_name = 0
+    else:
+        print('Directory found! Checking current number.')         
+        if not os.listdir(path) == []:
+            files = os.listdir(path)
+            full_list = [os.path.join(path,i) for i in files]
+            time_sorted_list = sorted(full_list, key=os.path.getmtime)
+            file_name = time_sorted_list[-1]
+            file_name = file_name.strip(path)
+            file_name = file_name.strip('\image_detection_training_')
+            file_name = file_name.strip('.png')
+            print('Starting frame count: ' + file_name)
+        else: 
+            print('Folder empty. Starting frame count: 1')
+            file_name = 0 
+            #Current frame           
+    return (file_name, path)
+
+def labelimg():
+    """
+    1: /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+       git clone https://github.com/tzutalin/labelImg.git
+    2: brew install qt  # will install qt-5.x.x
+    3: brew install libxml2
+    4: navigate to the labelImg directory
+    5: make qt5py3 
+    6: python3 labelImg.py
+    """
+    
+
+
+print("Starting Script...")
+# hook mouse
+hm = pyHook.HookManager()
+hm.SubscribeMouseLeftDown(left_down)
+hm.SubscribeMouseRightDown(right_down)
+
+print("Mouse Hooked. Hooking Keyboard.")
+# hook keyboard
+hm.KeyDown = OnKeyboardEvent # watch for all keyboard events
+
+
+print("Keyboard Hooked. Starting main.")
 main()
